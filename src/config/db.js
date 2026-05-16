9715355942bf73be4ee0d4798bf79c549f5e75f3
Rashid004 <rashidansari3868038@gmail.com>
2026-05-16 16:12:29 +0530

    refactor: restructure server initialization and enhance MongoDB connection logging

--- src/config/db.js
+++ src/config/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    console.log("Connecting to:", process.env.MONGO_URI ? process.env.MONGO_URI.substring(0, 40) + "..." : "URI MISSING");
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (err) {
--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
 import "dotenv/config";
+import connectDB from "./config/db.js";
 import app from "./app.js";
+
+import "./services/inventoryService.js";
+import "./services/emailService.js";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const start = async () => {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(`Server: http://localhost:${PORT}`);
+  });
+};
+
+start();
